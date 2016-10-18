43dda395bf3e9527d0f5bb58faf09fccbf2a069d
Oswaldo Osuna Millan <oosuna@ran.gob.mx>
2016-10-18 18:41:16 -0500

    Initial commit

---
+++ app/src/main/java/com/cystdi/proyectofinalappavanzadas/MainActivity.java
@@ -0,0 +1,28 @@
+package com.cystdi.proyectofinalappavanzadas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/*GitHub
+oswaldoosuna
+oz.osuna@gmail.com
+
+descargar : https://git-scm.com/downloads/
+
+https://www.youtube.com/watch?v=TSxL62AI6Wo
+
+El repositorio debe ser la carpeta donde se instaló Git y agregar /bin/ donde se ubica el archivo git.exe
+*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cargarInformacion(){
+
+    }
+}
